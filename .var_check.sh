16917ac1d4b660e0ab7177c8f820a6dd98351924
llucGil <lluc.gilperez@travelport.com>
2025-04-25 12:03:20 +0200

    add: start script and varibales check script

---
+++ .var_check.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/bash
+
+function check_env_var() {
+
+      if [[ -z $GH_TOKEN_ENV ]]; then
+            echo "[ERROR] \$GH_TOKEN is empty"
+            echo "Please set the GH_TOKEN environment variable"
+            exit 1
+      fi
+      if [[ -z $GH_ORG ]]; then
+            echo "[ERROR] \$GH_ORG is empty"
+            echo "Please set the GH_ORG environment variable"
+            exit 1
+      fi
+      if [[ -z $SLACK_CLIENT_ID ]]; then
+            echo "[ERROR] \$SLACK_CLIENT_ID is empty"
+            echo "Please set the SLACK_CLIENT_ID environment variable"
+            exit 1
+      fi
+      if [[ -z $SLACK_CLIENT_SECRET ]]; then
+            echo "[ERROR] \$SLACK_CLIENT_SECRET is empty"
+            echo "Please set the SLACK_CLIENT_SECRET environment variable"
+            exit 1
+      fi
+      if [[ -z $SLACK_SIGNING_SECRET ]]; then
+            echo "[ERROR] \$SLACK_SIGNING_SECRET is empty"
+            echo "Please set the SLACK_SIGNING_SECRET environment variable"
+            exit 1
+      fi      
+      if [[ -z $SLACK_APP_TOKEN ]]; then
+            echo "[ERROR] \$SLACK_APP_TOKEN is empty"
+            echo "Please set the SLACK_APP_TOKEN environment variable"
+            exit 1
+      fi    
+      if [[ -z $SLACK_BOT_TOKEN ]]; then
+            echo "[ERROR] \$SLACK_BOT_TOKEN is empty"
+            echo "Please set the SLACK_BOT_TOKEN environment variable"
+            exit 1
+      fi          
+      if [[ -z $SPAM_CHANNEL_ID ]]; then
+            echo "[ERROR] \$SPAM_CHANNEL_ID is empty"
+            echo "Please set the SPAM_CHANNEL_ID environment variable"
+            exit 1
+      fi      
+      if [[ -z $REDIS_URL ]]; then
+            echo "[ERROR] \$REDIS_URL is empty"
+            echo "Please set the REDIS_URL environment variable"
+            exit 1
+      fi      
+}
+
+check_env_var
