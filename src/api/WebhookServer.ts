b83103a37c57219207d833f02cbd9a14f0339c2c
picuu <dev@carloscapo.com>
2025-05-05 13:08:16 +0200

    feat: add respond method to WebhookServer class

--- src/api/WebhookServer.ts
+++ src/api/WebhookServer.ts
@@ -1,6 +1,5 @@
-import bodyParser from 'body-parser'
 import express from 'express'
-import type { Express } from 'express'
+import type { Express, Request, Response } from 'express'
 
 class WebhookServer {
   private app: Express
@@ -11,15 +10,20 @@ class WebhookServer {
     this.app = express()
     this.port = port
     this.path = path
+
+    this.app.use(express.json())
+
+    this.app.post(this.path, this.respond)
   }
 
-  private respond() {
+  private respond(req: Request, _: Response) {
+    const eventType = req.header('X-GitHub-Event')
 
+    // biome-ignore lint/suspicious/noConsole: This is a server log
+    console.log(`[Webhook Server] - ${eventType}: ${req.body.action}`)
   }
 
   run() {
-    this.app.use(bodyParser.json())
-
     this.app.listen(this.port, () => {
       // biome-ignore lint/suspicious/noConsole: This is a server log
       console.log(`Server is running on http://localhost:${this.port}`)
